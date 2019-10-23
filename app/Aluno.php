18bea2f9eb118eef2d12444645baf27ac9335555
wagcbjr <wagcbjr@gmail.com>
2019-10-23 18:14:39 +0000

    init

--- app/Aluno.php
+++ app/Aluno.php
@@ -7,4 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Aluno extends Model
 {
     protected $fillable = ['name'];
+
+    public function notas()
+    {
+        return $this->hasMany('App\Nota');
+    }
+
 }
--- app/Nota.php
+++ app/Nota.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Nota extends Model
 {
-    //
+    protected $fillable = ['aluno_id', 'materia', 'nota'];
+
+    public function aluno()
+    {
+        return $this->belongsTo(Aluno::class);
+    }
+
 }
