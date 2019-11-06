008f9a71c72329468ac492568930b3328e88007f
Wagner Felipe Leszkiewicz <wagcbjr@gmail.com>
2019-11-06 12:46:55 +0000

    Revert "auth routes with JWT"
    This reverts commit 4e9133718db0f31c48337d59f7ff0efa5260bfc0.

--- routes/api.php
+++ routes/api.php
@@ -13,25 +13,15 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::post('auth/register', 'AuthController@register');
-Route::post('auth/login', 'AuthController@login');
-
-Route::middleware('auth:api')->group(function ($e) {
-
-    Route::get('alunos', 'AlunoController@index');
-    Route::get('alunos/{aluno}', 'AlunoController@show');
-    Route::post('alunos', 'AlunoController@store');
-    Route::put('alunos/{aluno}', 'AlunoController@update');
-    Route::delete('alunos/{aluno}', 'AlunoController@destroy');
+Route::get('alunos', 'AlunoController@index');
+Route::get('alunos/{aluno}', 'AlunoController@show');
+Route::post('alunos', 'AlunoController@store');
+Route::put('alunos/{aluno}', 'AlunoController@update');
+Route::delete('alunos/{aluno}', 'AlunoController@destroy');
 
 
-    Route::post('alunos/{aluno}/notas', 'NotasController@store');
-    Route::get('alunos/{aluno}/notas', 'NotasController@index');
-    Route::get('alunos/{aluno}/notas/{nota}', 'NotasController@show');
-    Route::put('alunos/{aluno}/notas/{nota}', 'NotasController@update');
-    Route::delete('alunos/{aluno}/notas/{nota}', 'NotasController@destroy');
-
-    Route::post('auth/logout', 'AuthController@logout');
-    Route::get('auth', 'AuthController@index');
-
-});
+Route::post('alunos/{aluno}/notas', 'NotasController@store');
+Route::get('alunos/{aluno}/notas', 'NotasController@index');
+Route::get('alunos/{aluno}/notas/{nota}', 'NotasController@show');
+Route::put('alunos/{aluno}/notas/{nota}', 'NotasController@update');
+Route::delete('alunos/{aluno}/notas/{nota}', 'NotasController@destroy');
